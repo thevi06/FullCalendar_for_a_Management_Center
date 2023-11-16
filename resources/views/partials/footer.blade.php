03bae636db96ad9c2013d224bbd89cc3439c85df
Thevindu Dilmith <thevindu.20210197@iit.ac.lk>
2023-11-16 09:24:50 +0530

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="float-right d-none d-sm-block">
         <b>Version</b> 3.2.0
     </div>
-    <strong>Copyright &copy; 2023 <a href="https://www.slt.lk/" target="_blank"></a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2023. <a href="https://www.slt.lk/" target="_blank"></a>.</strong> All rights reserved.
 </footer>
 
 <!-- Control Sidebar -->
